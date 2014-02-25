2a0dd6c4455744c1048324491d7c199c7c32f1f9
boniface <boniface@kabaso.com>
2014-02-25 16:50:03 +0200

    Design Principles Template Exercise

---
+++ src/test/java/com/kabaso/designprinciples/ocp/TestOCPViolation.java
@@ -0,0 +1,18 @@
+package com.kabaso.designprinciples.ocp;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * Created by boniface on 2014/02/25.
+ */
+public class TestOCPViolation {
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+}
